36f72f1379c9a1f77808f076899df2dc0332d6fe
Sandra <dasainajo@gmail.com>
2024-02-14 15:56:38 +0100

    Fragments y Nav Bar
    Creacion de los fragments y la navigation Bar

--- app/src/main/java/com/scj/youcanfit/HomeActivity.java
+++ app/src/main/java/com/scj/youcanfit/HomeActivity.java
@@ -1,24 +1,39 @@
 package com.scj.youcanfit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.media.RouteListingPreference;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.io.*;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.scj.youcanfit.fragments.FirstFragment;
+import com.scj.youcanfit.fragments.SecondFragment;
+import com.scj.youcanfit.fragments.ThirdFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
 
     TextView email, provider;
     Button logOut;
     FirebaseUser user;
     FirebaseAuth auth;
+
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +43,10 @@ public class HomeActivity extends AppCompatActivity {
         email = findViewById(R.id.emailTextView);
         provider = findViewById(R.id.providerTextView);
         logOut = findViewById(R.id.logoutButton);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.firstFragment);
 
         if(user == null){ // verificamos si el usuario tiene la sesion iniciada, y en caso de que no lo este no envie a AuthActivity
             Toast.makeText(getApplicationContext(),"No hi ha cap sesió iniciada",Toast.LENGTH_SHORT).show();
@@ -49,5 +68,42 @@ public class HomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
+
+    FirstFragment primerFragment = new FirstFragment();
+    SecondFragment segundoFragment = new SecondFragment();
+    ThirdFragment tercerFragment= new ThirdFragment();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        switch (item.getItemId()){
+
+            case R.id.firstFragment:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameMain,primerFragment)
+                        .commit();
+                return true;
+
+            case  R.id.secondFragment:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameMain,segundoFragment)
+                        .commit();
+                return true;
+
+            case  R.id.thirdFragment:Fragment:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameMain,tercerFragment)
+                        .commit();
+                return true;
+        }
+
+        return false;
+    }
+
+
 }
---
+++ app/src/main/java/com/scj/youcanfit/fragments/FirstFragment.java
@@ -0,0 +1,27 @@
+package com.scj.youcanfit.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.scj.youcanfit.R;
+
+public class FirstFragment extends Fragment {
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+}
