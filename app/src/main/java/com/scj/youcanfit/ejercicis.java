77cccc6cd9c95a33d6f3864f2c2f36cbf3b4247e
jofa446 <JOFA446@vidalibarraquer.net>
2024-02-15 09:29:16 +0100

    a

---
+++ app/src/main/java/com/scj/youcanfit/ejercicis.java
@@ -0,0 +1,14 @@
+package com.scj.youcanfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ejercicis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicis);
+    }
+}
