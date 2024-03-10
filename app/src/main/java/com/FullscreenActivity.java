91ccfff6404d94a66c08a4f8ff689d2699a2beb0
Sandra <dasainajo@gmail.com>
2024-03-10 13:29:17 +0100

    actualizacio splash screen

--- app/src/main/java/com/FullscreenActivity.java
+++ app/src/main/java/com/FullscreenActivity.java
@@ -145,21 +145,7 @@ private ActivityFullscreenBinding binding;
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            // Delayed removal of status and navigation bar
-//            if (Build.VERSION.SDK_INT >= 30) {
-//                mContentView.getWindowInsetsController().hide(
-//                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
-//            } else {
-//                // Note that some of these constants are new as of API 16 (Jelly Bean)
-//                // and API 19 (KitKat). It is safe to use them, as they are inlined
-//                // at compile-time and do nothing on earlier devices.
-//                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-//            }
+
         }
     };
 
@@ -182,12 +168,7 @@ private ActivityFullscreenBinding binding;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
-            // Delayed display of UI elements
-//            ActionBar actionBar = getSupportActionBar();
-//            if (actionBar != null) {
-//                actionBar.show();
-//            }
-//            mControlsView.setVisibility(View.VISIBLE);
+
         }
     };
 
